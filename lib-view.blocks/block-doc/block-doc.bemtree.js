ec967a3df8b056bfeb4c45e06f887d30fafeac6d
Vitaly Harisov <vitaly@yandex-team.ru>
2017-03-25 20:44:46 +0300

    Change path to examples, use bem.info-data (upyachka)

--- lib-view.blocks/block-doc/block-doc.bemtree.js
+++ lib-view.blocks/block-doc/block-doc.bemtree.js
@@ -9,7 +9,7 @@ block('block-doc').content()(function() {
             var exampleName = chunk.split('/').pop(),
                 examplePath = path.resolve(data.outputLibFolder,
                     data.setName, exampleName),
-                pathToExample = chunk.replace('tmp/data', '.cache/bem-lib-site-data'), // FIXME: упячка
+                pathToExample = chunk.replace('tmp/data', 'node_modules/bem.info-data'), // FIXME: упячка
                 pathToHtml = path.resolve(pathToExample.replace(/\/(.*?)\.examples\//, '/$1.html/'));
 
             return {
